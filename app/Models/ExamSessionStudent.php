0daa3db8d5fdbfb1ec999dadcc78967ba71aa462
anan03082021 <anvotrantrk@gmail.com>
2026-03-18 23:47:15 +0700

    18/3/2026

--- app/Models/ExamSessionStudent.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class ExamSessionStudent extends Model
-{
-    protected $fillable = ['exam_session_id', 'student_email', 'student_name', 'user_id'];
-}
